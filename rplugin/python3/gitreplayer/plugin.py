90a7098533702871309520ec1f65e7c9f8f99006
mitchmcdee <mitchmcdee@gmail.com>
2018-09-01 03:23:39 +1000

    added commands for testing

--- rplugin/python3/gitreplayer/plugin.py
+++ rplugin/python3/gitreplayer/plugin.py
@@ -27,6 +27,15 @@ class GitReplayer:
     def __init__(self, nvim):
         self.nvim = nvim
 
+    @neovim.function('TestFunction', sync=True)
+    def testfunction(self, args):
+        return 3
+
+    @neovim.command('TestCommand', nargs='*', range='')
+    def testcommand(self, args, range):
+        self.nvim.current.line = ('Command with args: {}, range: {}'
+                                  .format(args, range))
+
     @neovim.autocmd('GitRepoEnter', pattern='*.git', sync=True)
     def on_git_repo_enter(self, filename):
         self.nvim.out_write('testplugin is in ' + filename + '\n')
