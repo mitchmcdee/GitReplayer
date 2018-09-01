470ac7907a042dc4ad23a4734785ee0c59ba8295
mitchmcdee <mitchmcdee@gmail.com>
2018-09-01 14:13:30 +1000

    simplified

--- rplugin/python3/gitreplayer/plugin.py
+++ rplugin/python3/gitreplayer/plugin.py
@@ -81,13 +81,14 @@ class GitReplayerPlugin:
         window = self.nvim.current.window
         height = window.height
         width = window.width
-        _, cursor_y = self.nvim.api.win_get_cursor(window)
+        _, cursor_y = window.cursor
         # Write out all chars in added line.
         for i in range(len(added_line)):
             self.nvim.current.buffer[line_num] = added_line[:i]
             wrapped_x = i % width
             wrapped_y = i // width
-            self.nvim.api.win_set_cursor(window, (wrapped_x, cursor_y + wrapped_y))
+            self.nvim.out_write(f'{wrapped_x}, {cursor_y + wrapped_y} | {width} {height}')
+            window.cursor = (wrapped_x, cursor_y + wrapped_y)
             time.sleep(1 / self.playback_speed)
 
     def handle_line_removal(self, line_num):
