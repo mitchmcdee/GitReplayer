fde98508aae132d166a70d7a51c3e91d2a2c3c4f
mitchmcdee <mitchmcdee@gmail.com>
2018-09-02 16:11:33 +1000

    or

--- rplugin/python3/gitreplayer/plugin.py
+++ rplugin/python3/gitreplayer/plugin.py
@@ -58,7 +58,7 @@ class GitReplayerPlugin:
             return
         # First timestep is initial state, rest are diffs.
         self.files = self.get_file_state_at_timestep(timeline[0])
-        print(self.files)
+        self.nvim.out_write(str(self.files))
         input()
         self.timeline = timeline[1:]
         self.replay()
